64e710c4f2fc6bf0511b3d26698a265c17252d3d
ArCaN01 <tga977@outlook.com>
2024-06-07 20:05:30 +0300

    1

---
+++ whisperapp/pages/_app.tsx
@@ -0,0 +1,36 @@
+import type { AppProps } from "next/app";
+// @ts-ignore
+import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, rainbowWallet, smartWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "mumbai";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  //Set up smart wallet config
+  const smartWalletConfig = {
+    factoryAddress: "0x26B2c66aA79B36AFF5393235DbB2F343d168544A",
+    gasless: true,
+  };
+  
+  return (
+    <ThirdwebProvider
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      activeChain={activeChain}
+      supportedWallets={[
+        smartWallet(embeddedWallet(), smartWalletConfig),
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        rainbowWallet(),
+        trustWallet(),
+      ]}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
